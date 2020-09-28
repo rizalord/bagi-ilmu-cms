746093f0eab0c5910c6145c6b395f783449804b1
rizalord <ahmadkhamdani9@gmail.com>
2020-09-28 09:39:54 +0700

    initial commit

---
+++ config/database.js
@@ -0,0 +1,27 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        schema: 'public',
+        ssl: false 
+      },
+      options: {
+        pool: {
+          min: 0,
+          max: 15,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000
+        }
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6494dc6-9db9-45bb-a46a-0a74939371d0'
+};
